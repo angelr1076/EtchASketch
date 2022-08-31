9cdc62acc2c02f4ba88d238f0089702d2940ea75
angelr1076 <angelr1076@gmail.com>
2022-08-31 17:15:49 -0400

    Updated logic and added styling

--- js/main.js
+++ js/main.js
@@ -1,34 +1,83 @@
-const grid = document.querySelector('#grid');
-const userInput = document.querySelector('#user-input');
-grid.style.fontSize = '1em';
-// Set pixel width and height
-let wdt = '1em';
-let hgt = '1em';
+let input = document.querySelector('.input');
+let cursorStatus = document.querySelector('.cursor-status');
+let black = document.querySelector('.black');
+let random = document.querySelector('.random');
+let erase = document.querySelector('.erase');
+let clear = document.querySelector('.clear');
+let penDown = true;
+let color = '#000'; // default to black
+let screenColor = '#d3d3d3';
 
-// Ask the user for the number of tiles for the sketch grid
-function getUserInput() {
-  let input = parseInt(prompt(`Please enter the grid size you'd like`));
-  input <= 100 || !isNaN(input)
-    ? createSketchGrid(input)
-    : alert('Please enter a valid number less than or equal to 100.');
+function renderBoard(size) {
+  let screen = document.querySelector('.screen');
+  screen.style.backgroundColor = screenColor;
+  let tiles = screen.querySelectorAll('div');
+  // Remove tiles before rerendering
+  tiles.forEach(tile => tile.remove());
+  // Set screen size evenly
+  screen.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  screen.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  screen.style.gridGap = '1px';
+
+  let screenSize = size * size;
+
+  for (let i = 0; i < screenSize; i++) {
+    let pixel = document.createElement('div');
+    screen.appendChild(pixel);
+    pixel.addEventListener('mouseover', drawOnCanvas);
+  }
 }
+renderBoard(input.value);
 
-// Event listener to create tiles in mouseover
-function setTiles(e) {
-  e.target.classList.toggle('fill');
-}
-
-// Create the grid
-function createSketchGrid(tiles) {
-  let gridSize = tiles * tiles;
-  for (let i = 0; i < gridSize; i++) {
-    let tile = document.createElement('div');
-    tile.style.width = wdt;
-    tile.style.height = hgt;
-    grid.appendChild(tile);
-
-    tile.addEventListener('mouseover', setTiles);
+function changeSize(entry) {
+  entry = this.value;
+  if (entry >= 4 && entry < 101) {
+    renderBoard(entry);
+  } else {
+    alert('Please enter a value > 3 or < 101');
+    input.value = '';
   }
 }
 
-userInput.addEventListener('click', getUserInput);
+function drawOnCanvas() {
+  if (penDown) {
+    this.style.backgroundColor = color;
+  }
+}
+
+function selectColor() {
+  color = this.value;
+  if (color === 'random') {
+    color !== screenColor && color !== '#fff';
+    color =
+      '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+  }
+  return color;
+}
+
+function clearBoard() {
+  let screen = document.querySelector('.screen');
+  let tiles = screen.querySelectorAll('div');
+  tiles.forEach(tile => tile.remove());
+  renderBoard(input.value);
+}
+
+input.addEventListener('change', changeSize);
+black.addEventListener('click', selectColor);
+random.addEventListener('click', selectColor);
+erase.addEventListener('click', selectColor);
+clear.addEventListener('click', clearBoard);
+
+document.querySelector('body').addEventListener('click', e => {
+  penDown = !penDown;
+  console.log(e.target.tagName);
+  if (e.target.tagName !== 'BUTTON' && e.target.tagName !== 'INPUT') {
+    if (penDown) {
+      cursorStatus.style.color = '#fff'; // Blue
+      cursorStatus.textContent = 'Pen: Active';
+    } else {
+      cursorStatus.style.color = '#000'; // Red
+      cursorStatus.textContent = 'Pen: Inactive';
+    }
+  }
+});
